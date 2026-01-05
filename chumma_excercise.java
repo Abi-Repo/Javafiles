9cec04d88dcc97581026867d8701fc6f538125c8
Abi <abinaya11758@gmail.com>
2026-01-05 22:35:46 +0530

    Add files via upload

---
+++ chumma_excercise.java
@@ -0,0 +1,140 @@
+package javakunal;
+
+//import java.util.Scanner;
+//public class chumma_excercise {
+//    static void main() {
+//        Scanner input = new Scanner(System.in);
+//        int number = input.nextInt();
+//        int count = 0;
+//        String string = String.valueOf(number);
+//        for(int i=0; i<string.length(); i++ ){
+//            int total = number%10;
+//            number /= 10;
+//            if (total == 0){
+//                count++;
+//            }
+//        }
+//        System.out.println(count);
+//    }
+//}
+
+
+//import java.util.Scanner;
+//
+//public class chumma_excercise {
+//    static void main() {
+//        Scanner input = new Scanner(System.in);
+//        int number = input.nextInt();
+//
+//        String string = String.valueOf(number);
+//        int output = 1;
+//
+//        for (int i=0; i<string.length(); i++){
+//            int remainder = number%10;
+//            number /=10;
+//            output *= remainder;
+//        }
+//        System.out.println(output);
+//    }
+//}
+
+
+//import java.util.Scanner;
+//
+//public class chumma_excercise {
+//    static void main() {
+//        Scanner input = new Scanner(System.in);
+//        int number = input.nextInt();
+//
+//        int original = number;
+//        int copy = 0;
+//        String string = String.valueOf(number);
+//
+//        for (int i=0; i<string.length(); i++){
+//            int remainder = number%10;
+//            int cube = ((remainder*remainder)*remainder);
+//            copy += cube;
+//            number /= 10;
+//        }
+//        if (original == copy){
+//            System.out.println("it is an armstrong number");
+//        }else {
+//            System.out.println("it is not an armstrong number");
+//        }
+//    }
+//}
+
+
+import java.util.Scanner;
+
+//public class chumma_excercise {
+//    static void main() {
+//        Scanner input = new Scanner(System.in);
+//        int number = input.nextInt();
+//        int digit = input.nextInt();
+//
+//        String string = String.valueOf(number);
+//        for (int i=0; i<string.length(); i++){
+//            int remainder = number%10;
+//            number/=10;
+//            if(remainder==digit){
+//                System.out.println("the number is found");
+//                break;
+//            }
+//        }
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ switchandnested.java
@@ -0,0 +1,167 @@
+package javakunal;
+
+import java.util.Scanner;
+
+//public class switchandnested {
+//    static void main() {
+//        String a = "abi";
+//        String b = "abi";
+//        //i'm telling to check whether a is equal to b or what.
+//        System.out.println(a.equals(b));
+//    }
+//}
+
+//public class switchandnested {
+//    static void main() {
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("enter the number : ");
+//        int number = input.nextInt();
+//
+//        switch (number){
+//            case 1 :
+//                System.out.println("sunday");
+//                break;
+//            case 2 :
+//                System.out.println("monday");
+//                break;
+//            case 3 :
+//                System.out.println("tuesday");
+//                break;
+//            case 4:
+//                System.out.println("wednesday");
+//                break;
+//            case 5 :
+//                System.out.println("thursday");
+//                break;
+//            case 6:
+//                System.out.println("friday");
+//                break;
+//            case 7 :
+//                System.out.println("saturday");
+//                break;
+//            default:
+//                System.out.println("avlothan");
+//        }
+//
+//    }
+//}
+
+
+//public class switchandnested {
+//    static void main() {
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.print("enter the number 1 : ");
+//        int number1 = input.nextInt();
+//        System.out.print("enter the number 2 : ");
+//        int number2 = input.nextInt();
+//
+//        System.out.print("enter the operator : ");
+//        String string = input.next();
+//
+//        switch (string){
+//            case "+":
+//                System.out.println(number1 + number2);
+//                break;
+//            case "-":
+//                System.out.println(number1 - number2);
+//                break;
+//            case "*" :
+//                System.out.println(number1 * number2);
+//                break;
+//            case "/" :
+//                System.out.println(number1/number2);
+//                break;
+//            default:
+//                System.out.println("program in case 4 (/) division has got some error");
+//        }
+//    }
+//}
+
+
+////nested switch case
+//public class switchandnested {
+//    static void main() {
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.println("enter the year : ");
+//        int year = input.nextInt();
+//        System.out.println("enter the month : ");
+//        int month = input.nextInt();
+//        System.out.println("enter the day : ");
+//        int day = input.nextInt();
+//
+//        switch (year){
+//            case 1 :
+//                System.out.println("this is 2026 year");
+//            switch (month){
+//                case 1 :
+//                    System.out.println("this is the month of january");
+//                    break;
+//                case 2 :
+//                    System.out.println("this is the month of february");
+//                    break;
+//            }
+//        }
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
